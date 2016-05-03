e384f003833953f6868909fe8583dd64d34dc365
Dimitry Besson <dimitrybesson@gmail.com>
2016-05-03 18:05:26 -0400

    Filled out most of projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,42 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    # set user_id to current_user
+    if @project.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      redirect_to project_url(@project)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :description, :goal, :start_time, :end_time)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'projects#index'
 
 
   resources :users, except: :index
