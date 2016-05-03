55ca3087775f60c8ba3410f762a58e83dab3c7db
Dimitry Besson <dimitrybesson@gmail.com>
2016-05-03 17:52:56 -0400

    Generated controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+
+
+  resources :users, except: :index
+  resources :projects do
+    resources :pledges, except: %i(edit update)
+    resources :rewards
+  end
+  resources :sessions, only: %i(new create destroy)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
