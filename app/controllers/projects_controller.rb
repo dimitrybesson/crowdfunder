b4cc56e7a3441d380c013108a16a2daf536960d9
Joshua Rider <joshrider@protonmail.ch>
2016-05-03 19:17:59 -0400

    projectscontroller, rewards controller, has_many associations, beginning of views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,11 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i(show edit update destroy)
 
   def index
     @projects = Project.all
+  end
+
+  def show
   end
 
   def new
@@ -12,18 +16,16 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
     # set user_id to current_user
     if @project.save
-      redirect_to root_url
+      redirect_to project_url(@project)
     else
       render :new
     end
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update_attributes(project_params)
       redirect_to project_url(@project)
     else
@@ -32,11 +34,17 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-
+    @restaurant.destroy
+    redirect_to projects_url
   end
 
   private
+  
   def project_params
     params.require(:project).permit(:name, :description, :goal, :start_time, :end_time)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
