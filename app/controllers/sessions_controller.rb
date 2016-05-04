ec39505ae22997ea6cbba461aa7a1434102b6bc8
Dimitry Besson <dimitrybesson@gmail.com>
2016-05-04 15:33:37 -0400

    Filled out sessions controller and related views and links, set up current user helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged in successfully!"
+    else
+      render :new, alert: "Failure to log in"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Log out successful!"
   end
 end
